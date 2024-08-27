d7483dec86fb1992c3c81a251de39ed6919d6897
snoring-dev <jemmoudi.med@proton.me>
2024-08-27 18:54:45 +0200

    fix(app): enabling cors for all apps

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,14 @@ async function importWords(): Promise<void> {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   // await importWords();
